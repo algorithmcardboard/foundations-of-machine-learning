0c1645f3bcf2151dd8caddc438e82898448077c5
Anirudhan J Rajagopalan <ajr619@nyu.edu>
2015-11-07 13:34:57 -0800

    initial changes for cross_validation script

--- hw2/libsvm-3.20/tools/cross_validation.py
+++ hw2/libsvm-3.20/tools/cross_validation.py
@@ -1,14 +1,22 @@
 import numpy as np
+import os, sys, traceback, getpass, time, re
+from threading import Thread
+from subprocess import *
 
 cross_validation_pairs = {
-'train1':'file10',
-'train2':'file1',
-'train3':'file2',
-'train4':'file3',
-'train5':'file4',
-'train6':'file5',
-'train7':'file6',
-'train8':'file7',
-'train9':'file8',
-'train10':'file9'
+1:'file10',
+2:'file1',
+3:'file2',
+4:'file3',
+5:'file4',
+6:'file5',
+7:'file6',
+8:'file7',
+9:'file8',
+10:'file9'
 }
+
+for training_set, holdout_set in sorted(cross_validation_pairs.items()):
+    training_set = "train" + str(training_set)
+
+    print training_set, holdout_set
