7b7636c33b627bb478ed11fe4779141aeb4a1c12
Anirudhan J Rajagopalan <ajr619@nyu.edu>
2015-11-07 20:56:51 -0800

    wrapping up d = 1 case

--- hw2/libsvm-3.20/tools/cross_validation.py
+++ hw2/libsvm-3.20/tools/cross_validation.py
@@ -10,11 +10,13 @@ TOOLS_PATH = "/home/anirudhan/workspace/foudnations-of-machine-learning/hw2/libs
 DATASET_PATH = TOOLS_PATH + "partitions/"
 MODEL_PATH = DATASET_PATH + "models/"
 
-C_START = -8
+C_START = -6 
 C_STEP = 0.5
 C_END = 6 + C_STEP #By default np.arange doesn't include the c_stop
 
 KERNEL_DEGREE = 1
+
+ACCURACY_REGEX = re.compile("Accuracy = ([\d]+\.[\d]+)")
 
 cross_validation_pairs = {
         1:'file10',
@@ -30,13 +32,28 @@ cross_validation_pairs = {
         }
 
 for c in np.arange(C_START, C_END, C_STEP):
-    for training_set, holdout_set in sorted(cross_validation_pairs.items()):
-        training_set = "train" + str(training_set)
+    for training_id, holdout_set in sorted(cross_validation_pairs.items()):
+        training_set = "train" + str(training_id)
 
-        model_file = "c_{0}_d_{1}.{2}.model".format(c if c >=0 else "_"+str(abs(c)), KERNEL_DEGREE, training_set)
+        accuracy = 0;
+
+        pattern = 'c_{0}_d_{1}.{2}'.format(c if c >=0 else "_"+str(abs(c)), KERNEL_DEGREE, training_set)
+
+        model_file = "{0}.model".format(pattern)
+        out_file = '{0}.out'.format(pattern)
 
         cmd = '{0}/svm-train -t 1 -c {1} -d {2} {3} {4}'.format(LIBSVM_PATH, 5**c, KERNEL_DEGREE, DATASET_PATH+training_set, MODEL_PATH+model_file)
 
         result = Popen(cmd,shell=True,stdout=PIPE,stderr=PIPE,stdin=PIPE).stdout
         for line in result.readlines():
             print(line),
+
+        predict_cmd = '{0}/svm-predict {1} {2} {3}'.format(LIBSVM_PATH, DATASET_PATH+holdout_set, MODEL_PATH+model_file, MODEL_PATH+out_file)
+
+        predict_result = Popen(predict_cmd,shell=True,stdout=PIPE,stderr=PIPE,stdin=PIPE).stdout
+        for line in predict_result.readlines():
+            print(line),
+            if(len(ACCURACY_REGEX.match(line).groups())):
+                accuracy = float(ACCURACY_REGEX.match(line).groups()[0])
+
+        print "OURRESULT {0} {1} {2} {3} {4}".format(KERNEL_DEGREE, c, training_set, holdout_set, accuracy)
