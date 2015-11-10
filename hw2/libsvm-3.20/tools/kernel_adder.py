b0fef249e464d6be0281c2ea79b44b4cf113834d
Anirudhan J Rajagopalan <ajr619@nyu.edu>
2015-11-09 19:25:13 -0800

    summing gaussian and poly kernels

--- hw2/libsvm-3.20/tools/kernel_adder.py
+++ hw2/libsvm-3.20/tools/kernel_adder.py
@@ -1,8 +1,8 @@
-import pandas as pd
-import math
 from sklearn import datasets
-import numpy.linalg as LA
+from scipy.spatial.distance import pdist, squareform
+import scipy as scip
 import numpy as np
+import math
 
 SPLICE_LOCATION = "/home/anirudhan/workspace/foudnations-of-machine-learning/hw2/libsvm-3.20/tools/splice_hw/"
 FILE_NAME = "splice_noise_train.txt.scale"
@@ -15,27 +15,29 @@ sigma = math.sqrt(1/(2 * GAMMA))
 negSigmaSq = sigma * sigma * -1;
 
 X, Y = datasets.load_svmlight_file(SPLICE_LOCATION+FILE_NAME)
+X = X.toarray()
 
-SUM = np.empty([X.shape[0], X.shape[0]+1])
-GAUSSIAN = np.empty([X.shape[0], X.shape[0]+1])
-POLY = np.empty([X.shape[0], X.shape[0]+1])
+gamma = 1.0/X.shape[1]
+pairwise_dists = squareform(pdist(X, 'euclidean'))
 
-for i in range(0,X.shape[0]):
+GAUSSIAN = scip.exp(pairwise_dists ** 2 / negSigmaSq)
+GAUSSIAN = np.insert(GAUSSIAN, 0, np.arange(1, X.shape[0]+1), axis=1)
 
-    SUM[i,0] = i+1
-    GAUSSIAN[i,0] = i+1
-    POLY[i,0] = i+1
+POLY = np.dot(X, X.T)
+POLY = np.multiply(POLY, gamma)
+POLY = np.power(POLY, DEGREE)
+POLY = np.insert(POLY, 0, np.arange(1, X.shape[0]+1), axis=1)
 
-    for j in range(0,X.shape[0]):
-        polynomial = (X[i].dot(X[j].transpose()) ** DEGREE )[0,0]
-        gaussian = math.exp(LA.norm( (X[j] - X[i]).data, ord=2)**2/negSigmaSq)
-        total = polynomial + gaussian
+print "shape of poly is {0}".format(POLY.shape)
 
-        print "computed for [{0} ,{1}] = {2} {3} {4}".format(i, j, total, polynomial, gaussian)
-        SUM[i, j+1] = total
-        POLY[i, j+1] = polynomial
-        GAUSSIAN[i, j+1] = gaussian
+print "shape of gaussian is {0}".format(GAUSSIAN.shape)
+
+print "found gaussian and poly"
+SUM = GAUSSIAN + POLY
 
 datasets.dump_svmlight_file(SUM, Y, 'ques6.sum.kernel')
+print "finished dumping sum kernel"
 datasets.dump_svmlight_file(GAUSSIAN, Y, 'ques6.gaussian.kernel')
+print "finished dumping gaussian kernel"
 datasets.dump_svmlight_file(POLY, Y, 'ques6.polynomial.kernel')
+print "finished dumping polynomial kernel"
